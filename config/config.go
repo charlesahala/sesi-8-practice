a91ee200627b45a0543b209a1bff5e167ac88281
Charles Sahala <112320242+charlesahala@users.noreply.github.com>
2022-10-05 22:07:46 +0700

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"sesi-8-practice/structs"
+	"github.com/jinzhu/gorm"
+)
+
+//connection to database
+func DBInit() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/godb?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	db.AutoMigrate(structs.Person{})
+	return db
+}
---
+++ controller/gorm.go
@@ -0,0 +1,9 @@
+package struct
+
+import "github.com/jinzhu/gorm"
+
+type Person struct {
+	gorm.Model
+	First_Name string
+	Last_Name string
+}
